07c265f267dbbbffa1db955c2545fc578e0dd249
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-06-28 22:35:14 +0000

    First draft

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return(
+		<div className="container">
+			<h1>To Do List</h1>
+			<ul>
+				<li><input type="text" placeholder="Insert your task"></input></li>
+				<li>Take breakfast <i class="fas fa-tash-alt"></i></li>
+				<li>Feed my dog <i className="fas fa-tash-alt"></i></li>
+				<li>Drive to work <i className="fas fa-tash-alt"></i></li>
+				<li>Sleep in my bed <i className="fas fa-tash-alt"></i></li>
+			</ul>
+			<div>23 task</div>
 		</div>
 	);
 };
 
 export default Home;
+
